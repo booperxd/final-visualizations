eb54adc05c672b34244eba5ee776aff1f544b8d2
booperxd <coopervohs@gmail.com>
2021-12-10 01:44:08 -0500

    Finished tesla and musk graph??

--- muskteslaexpress.py
+++ muskteslaexpress.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import yfinance as yf
+import plotly.express as px
 import plotly.graph_objects as go
 
 tsla = yf.download('TSLA', period = '18d', interval='1d')
@@ -14,10 +15,10 @@ tweets_per_day(musktweets)
 
 freq = list(musktweets['created_at'].value_counts())
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(y=tsla['Close'], mode = 'lines'))
-fig.add_trace(go.Scatter(y=tsla['Close'], mode = 'markers', size=freq))
+fig1 = px.scatter(tsla,y='Close', size = freq)
+fig2 = px.line(tsla,y='Close')
 
-fig.update_layout(title = 'TSLA Data', yaxis_title = 'Close price', xaxis_title = 'Days')
+fig3 = go.Figure(data=fig1.data + fig2.data)
+fig3.update_layout(title = 'TSLA Data', yaxis_title = 'Close price', xaxis_title = 'Days')
 
-fig.show()
+fig3.show()
